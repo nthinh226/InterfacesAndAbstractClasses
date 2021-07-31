4117660cca4fa56e3124c870cc1e865734cdee46
Ngoc Thinh <78062673+nthinh226@users.noreply.github.com>
2021-07-31 22:47:56 +0700

    Update Main.java
    them tren web

--- src/tnt/nthinh/interfaceandabstractclasses/Main.java
+++ src/tnt/nthinh/interfaceandabstractclasses/Main.java
@@ -8,5 +8,6 @@ public class Main {
         carInterface.move(90);
         System.out.println("hihi");
         System.out.println("Moi71 them vo ne");
+        System.out.println("Cai nay the, tren magn ne");        
     }
 }
