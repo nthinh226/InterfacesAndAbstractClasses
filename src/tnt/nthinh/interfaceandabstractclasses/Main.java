a626ecd2e2afb963fad4b22fd10af3b19c162b4e
ngocthinh <ngocthinh1126@gmail.com>
2021-07-31 22:47:07 +0700

    moi them vo

--- src/tnt/nthinh/interfaceandabstractclasses/Main.java
+++ src/tnt/nthinh/interfaceandabstractclasses/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         CarInterface carInterface = new ElectricCar("Tesla");
         carInterface.start();
-
+        carInterface.move(60);
+        carInterface.move(90);
+        System.out.println("hihi");
+        System.out.println("Moi71 them vo ne");
     }
 }
